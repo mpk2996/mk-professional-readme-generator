49bfc419b808cfc0baa9aeb0a56e39ad18850ad1
Mike Kehoe <mkehoe2996@gmail.com>
2023-05-23 11:08:17 -0400

    updated index.js (incomplete), installed inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -7,7 +7,7 @@ const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'What is the title of your project? (Required)',
+        message: 'What is the title of your project?',
         validate: titleInput => {
             if (titleInput) {
                 return true;
@@ -20,7 +20,7 @@ const questions = [
     {
         type: 'input',
         name: 'githubUsername',
-        message: 'What is your GitHub Username? (Required)',
+        message: 'What is your GitHub Username?',
         validate: githubInput => {
             if (githubInput) {
                 return true;
@@ -33,7 +33,7 @@ const questions = [
     {
         type: 'input',
         name: 'email',
-        message: 'What is your email address? (Required)',
+        message: 'What is your email address?',
         validate: githubInput => {
             if (githubInput) {
                 return true;
@@ -45,36 +45,10 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'what',
-        message: 'What is your project and what problem will it solve? (Required)',
-        validate: whatInput => {
-            if (whatInput) {
-                return true;
-            } else {
-                console.log('Please enter what your project is!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'why',
-        message: 'Why did you create this project? (Required)',
-        validate: whyInput => {
-            if (whyInput) {
-                return true;
-            } else {
-                console.log('Please enter why you created this project!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'how',
-        message: 'How will someone use this? (Required)',
-        validate: howInput => {
-            if (howInput) {
+        name: 'description',
+        message: 'Please provide a summary of your project and its user story.',
+        validate: description => {
+            if (description) {
                 return true;
             } else {
                 console.log('Please enter what your project is!');
@@ -85,7 +59,7 @@ const questions = [
     {
         type: 'input',
         name: 'installation',
-        message: 'Please provide step-by-step installation instructions for your project. (Required)',
+        message: 'Please provide step-by-step installation instructions for your project.',
         validate: installInput => {
             if (installInput) {
                 return true;
@@ -98,7 +72,7 @@ const questions = [
     {
         type: 'input',
         name: 'usage',
-        message: 'Please provide instructions and examples for use. (Required)',
+        message: 'Please provide instructions and examples for use.',
         validate: usageInput => {
             if (usageInput) {
                 return true;
@@ -112,30 +86,31 @@ const questions = [
         type: 'list',
         name: 'license',
         message: 'Which license will you use for your project?',
-        choices: ['agpl', 'apache', 'mit', 'no license']
+        
+        choices: ['apache 2.0', 'mit', 'gpl 3.0']
     },
     {
         type: 'confirm',
-        name: 'confirmContributers',
-        message: 'Would you like to allow other developers to contribute?',
+        name: 'confirmContributors',
+        message: 'Would you like other users to be able to contribute?',
         default: true
     },
     {
         type: 'input',
         name: 'contribute',
-        message: 'Please provide guidelines for contributing. (Required)',
-        when: ({ confirmContributers }) => {
-            if (confirmContributers) {
+        message: 'Please provide guidelines for contributing.',
+        when: ({ confirmContributors }) => {
+            if (confirmContributors) {
                 return true;
             } else {
                 return false;
             }
         },
-        validate: contributerInput => {
-            if (contributerInput) {
+        validate: contributorInput => {
+            if (contributorInput) {
                 return true;
             } else {
-                console.log('Please enter contributer guidelines!');
+                console.log('Please enter contributor guidelines!');
                 return false;
             }
         }
@@ -143,12 +118,12 @@ const questions = [
     {
         type: 'input',
         name: 'test',
-        message: 'Please provide instructions on how to test the app. (Required)',
+        message: 'Please provide instructions on how to test the app.',
         validate: testInput => {
             if (testInput) {
                 return true;
             } else {
-                console.log('Please enter your use test instructions!');
+                console.log('Please instructions on how to test the app!');
                 return false;
             }
         }
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,88 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
+  let badge = '';
 
-// TODO: Create a function that returns the license link
+  switch (license.toLowerCase()) {
+    case 'mit':
+      badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      break;
+    case 'apache 2.0':
+      badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      break;
+    case 'gpl 3.0':
+      badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+      break;
+    default:
+      badge = '';
+      break;
+  }
+  return badge;
+}
+
+// Function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
 
-// TODO: Create a function that returns the license section of README
+  return `[License](https://opensource.org/licenses/${license})`;
+
+}
+
+// Function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
 
-// TODO: Create a function to generate markdown for README
+  return `## License
+
+This project is licensed under the ${license} license. Click [here](${renderLicenseLink(license)}) for more information.`;
+}
+
+// Function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} ${renderLicenseBadge(data.license)}
 
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+  
+  ## Contributing
+  ${renderContributingSection(data.confirmContributors, data.contribute)}
+  
+  ## Tests
+  ${data.test}
+  
+  
+  ## Questions
+  
+  If you have any questions, feel free to reach out:
+  
+  [GitHub] (https://github.com/${data.githubUsername})
+  [Email: ${data.email}](mailto:${data.email})
 `;
 }
 
